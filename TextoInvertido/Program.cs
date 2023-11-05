c46584fa8312142156bb2efad83473fbf46aeb01
Alex Cardozo <alex.cardozo1910@hotmail.com>
2023-11-05 17:57:29 -0300

    Adicionar arquivos de projeto.

---
+++ TextoInvertido/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TextoInvertido
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite o texto que deseja inverter:");
+            string texto = Console.ReadLine();
+
+            string textoInvertido = InverterLetras(texto);
+            Console.WriteLine("Texto invertido: " + textoInvertido);
+        }
+
+        static string InverterLetras(string input)
+        {
+            char[] caracteres = input.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+    }
+}
